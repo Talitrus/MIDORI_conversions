67f0560a5b64df904710d784e5e611b289e07bc6
Bryan Nguyen <nguyenbryann@gmail.com>
2020-09-10 15:27:09 -0400

    add BLCA scripts

---
+++ gen_midori_blca_tax.R
@@ -0,0 +1,25 @@
+#!/usr/bin/env Rscript
+
+
+# Setup ------------------
+library("taxonomizr")
+
+# Taxonomizr prep ----------------------------
+#prepareDatabase('accessionTaxa.sql')
+#assume database already prepared
+accnos <- scan(file = "midori_blca/midori_accnos.txt", what = character())
+accnos.1 <- paste0(accnos, ".1")
+taxaIds <- accessionToTaxa(accnos.1, "./accessionTaxa.sql")
+
+# Including the kingdom level here. Can we get BLCA to work with a kingdom level?
+lineages <- getTaxonomy(taxaIds, 'accessionTaxa.sql', desiredTaxa = c("superkingdom", "kingdom","phylum", "class", "order", "family", "genus", "species"), mc.cores = parallel::detectCores())
+tax.df <- as.data.frame(lineages, row.names = accnos)
+tax.df$taxID <- taxaIds
+
+uniq.tax.df <- tax.df[unique(row.names(tax.df)),]
+zz <- file("midori_blca/midori_kingdom_blca_taxonomy.txt", "w")
+for (xi in 1:nrow(uniq.tax.df)) {
+  writeLines(as.character(paste0(rownames(tax.df)[xi], "\t", "species:", tax.df$species[xi], ";genus:", tax.df$genus[xi], ";family:", tax.df$family[xi], ";order:", tax.df$order[xi], ";class:", tax.df$class[xi], ";phylum:", tax.df$phylum[xi], ";kingdom:", tax.df$kingdom[xi], ";superkingdom:", tax.df$superkingdom[xi],';')), con = zz)
+}
+
+close(zz)
---
+++ reformat_midori_to_blca.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#!/bin/bash
+#SBATCH -J to_BLCA
+#SBATCH -o out_err_files/MIDORI_to_BLCA_%A.out
+#SBATCH -e out_err_files/MIDORI_to_BLCA_%A.err
+#SBATCH --nodes=1
+#SBATCH -t 24:00:00
+#SBATCH -p short,defq
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=bnguyen@gwu.edu
+
+module load blast+/2.5.0
+module load R/3.4.2
+
+MIDORI_file="MIDORI_UNIQ_GB239_CO1_RAW.fasta"
+
+grep ">" $MIDORI_file | sed -E 's/root.*$//' | sed 's/>//' | uniq > midori_blca/midori_accnos.txt
+
+#Are there any duplicates?
+sed -E 's/root.*$//' $MIDORI_file | awk '/^>/{f=!d[$1];d[$1]=1}f' > midori_blca/midori_blca_dedup.fasta
+
+TMPDIR='.' Rscript gen_midori_blca_tax.R
+
+makeblastdb -in midori_blca/midori_blca_dedup.fasta -dbtype nucl -parse_seqids -out midori_blca/midori_blca_blast
+
+#makeblastdb -in $MIDORI_file  -dbtype nucl -parse_seqids -out midori_blca/midori_blca_blast
